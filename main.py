5907b06ce71a5a05047fef0ff2a7f1312b69f83b
Susanna Kayed <kefka.sk@gmail.com>
2024-02-28 07:22:17 +0100

    added function in generate password button

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from tkinter import * 
 from tkinter import messagebox
+from random import randint, shuffle, choice
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def gen_psw():
 
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_list=[]
+
+    password_list += [ choice(letters) for char in range(randint(8,10))]
+    password_list += [ choice(symbols) for char in range(randint(2,4))]
+    password_list += [ choice(numbers) for char in range(randint(2,4))]
+
+    
+    
+    password = "".join(password_list)
+    print(password)
+
+    return password
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save_to_txt():
@@ -54,7 +72,7 @@ password_label.grid(column=0,row=3)
 password_entry = Entry(width=21)
 password_entry.grid(column=1,row=3)
 
-password_button = Button(text="Generate Password")
+password_button = Button(text="Generate Password", command=gen_psw)
 password_button.grid(column=2, row=3)
 
 save_button = Button(text="Save", width=40, command=save_to_txt)
